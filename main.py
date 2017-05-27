e993a866f111c5378fe1d502f6da43948a625758
ivaylospasov <ivaylo.pif@gmail.com>
2017-05-27 18:10:24 +0300

    Get all filenames

--- main.py
+++ main.py
@@ -1,9 +1,27 @@
 #!/usr/bin/env python3
 
+from sys import argv
+from os import path, getcwd, remove, listdir
+
 # Convert document to txt file
 # soffice --convert-to txt filename.doc
 
+def list_files():
+    current_dir = getcwd()
+    yield listdir(current_dir)
 
+'''
 def convert_to_txt(files):
     for file in files:
         soffice --convert-to txt file
+    pass
+'''
+
+def all_names():
+    for file in list_files():
+        print(file)
+
+
+if __name__ == '__main__':
+    #main()
+    all_names()
