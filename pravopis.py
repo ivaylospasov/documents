8ad011a1a3838d6f1c30c4fd372506c15dfcd72f
ivaylospasov <ivaylo.pif@gmail.com>
2017-05-28 10:35:04 +0300

    Remove unused lines of code

--- pravopis.py
+++ pravopis.py
@@ -1,20 +1,14 @@
 #!/usr/bin/env python3
 
 from sys import argv
-from os import path, getcwd, remove, listdir
-import glob
 import subprocess
-#import re
+import glob
 
 def list_doc_files():
     list_of_all_docs = glob.glob('*.doc*')
     return list_of_all_docs
 
 def remove_additional_spaces():
-    '''
-    Use re just for spaces, but not for tabs
-    correct_text = re.sub(' +', ' ', text)
-    '''
     text = ' The       quick brown    fox'
     correct_text = " ".join(text.split())
     return correct_text
