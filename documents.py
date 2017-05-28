9c514aff798d34bed0fdd621c1aa37f832352643
ivaylospasov <ivaylo.pif@gmail.com>
2017-05-28 11:45:56 +0300

    Get filename without extension and add read file.txt function

--- documents.py
+++ documents.py
@@ -1,9 +1,15 @@
 #!/usr/bin/env python3
 
+from os import path
 from sys import argv
 import subprocess
 import glob
 
+
+def get_filename(filename):
+    file_name, file_extension = path.splitext(filename)
+    txt_file = file_name + ".txt"
+    return txt_file
 
 def remove_additional_spaces():
     text = 'Подмяната			 на     батерии продължава '
@@ -11,10 +17,16 @@ def remove_additional_spaces():
     print(correct_text)
     return correct_text
 
-def main(script, *argv):
-    for file in argv:
-        subprocess.run(['soffice', '--convert-to', 'txt', file])
+def main(file_name):
+    subprocess.run(['soffice', '--convert-to', 'txt', file_name])
+
+def read_file(file):
+    text_file = get_filename(file)
+    with open(text_file, 'r') as f:
+        read_data = f.read()
+    return read_data
 
 
 if __name__ == '__main__':
-    main(*argv)
+    main(argv[1])
+    print(read_file(argv[1]))
