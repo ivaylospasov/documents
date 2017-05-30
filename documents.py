aed12bfd49f0c64d1ae0f1a19499a46ba41af131
ivaylospasov <ivaylo.pif@gmail.com>
2017-05-30 20:04:47 +0300

    Finished function for removing additional spaces

--- documents.py
+++ documents.py
@@ -15,30 +15,17 @@ txt_file = get_filename(argv[1]) + '.' + txt_extension
 def convert_to_txt(file):
     subprocess.run(['soffice', '--convert-to', txt_extension, file])
 
-def read_file(file):
-    with open(txt_file, 'r') as f:
-        read_data = f.readlines()
-    return read_data
-
-def remove_additional_spaces(text):
-    text_for_correction = read_file(text)
-    for line in text_for_correction:
-        line = " ".join(line.split())
-        print(line)
-
-# def read_and_write_file(file):
-#     with open(file, 'r+', newline='\n') as f:
-#         f_content = f.readlines()
-#         #print(f_content)
-#         for line in f_content:
-#             line = " ".join(line.split())
-#             print(line)
-#             #f_content = f.readline()
+def read_and_write_file(file):
+    with open(file, 'r') as rf:
+        txt_file_ready = get_filename(file) + '_ready.' + txt_extension
+        print(txt_file_ready)
+        with open(txt_file_ready, 'w') as wf:
+            for line in rf:
+                line = " ".join(line.split())
+                wf.write(line + '\n')
 
 
 if __name__ == '__main__':
     get_filename(argv[1])
     convert_to_txt(argv[1])
-    remove_additional_spaces(argv[1])
-    read_file(argv[1])
-    #read_and_write_file(txt_file)
+    read_and_write_file(txt_file)
