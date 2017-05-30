b0821095590d8c2206e9835b9b3de23bc9611cde
ivaylospasov <ivaylo.pif@gmail.com>
2017-05-30 07:19:58 +0300

    refactoring some code

--- documents.py
+++ documents.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from os import path
+from os import path, renames
 from sys import argv
 import subprocess
 import glob
@@ -8,14 +8,13 @@ import glob
 
 def get_filename(filename):
     file_name, file_extension = path.splitext(filename)
-    txt_file = file_name + ".txt"
-    return txt_file
+    return file_name
 
-def convert_to_txt(file_name):
-    subprocess.run(['soffice', '--convert-to', 'txt', file_name])
+def convert_to_txt(file):
+    subprocess.run(['soffice', '--convert-to', 'txt', file])
 
 def read_file(file):
-    text_file = get_filename(file)
+    text_file = get_filename(file) + '.txt'
     with open(text_file, 'r') as f:
         read_data = f.readlines()
     return read_data
@@ -28,6 +27,7 @@ def remove_additional_spaces(text):
 
 
 if __name__ == '__main__':
+    get_filename(argv[1])
     convert_to_txt(argv[1])
     remove_additional_spaces(argv[1])
     #read_file(argv[1])
